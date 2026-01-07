4540d4dfbe033fa4aee417335b6ac124a52ca78d
You-Ri Su <suyourice@gmail.com>
2026-01-07 22:30:31 +0100

    add ROS 2 workspace layout and minimal dodo_ros_control package skeleton

---
+++ ros2_ws/src/dodo_ros_control/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+package_name = 'dodo_ros_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='ROS 2 runtime for sim2sim and sim2real control of the Dodo robot.',
+    license='TODO',
+    entry_points={
+        'console_scripts': [],
+    },
+)
