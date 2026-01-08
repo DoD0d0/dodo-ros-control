4179fb1d5a3788b3a60ea0753a971f43d3abc9dd
You-Ri Su <suyourice@gmail.com>
2026-01-08 04:23:03 +0100

    initialize ROS2 Python package for dodo_ros_control

--- ros2_ws/src/dodo_ros_control/setup.py
+++ ros2_ws/src/dodo_ros_control/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'dodo_ros_control'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -18,6 +18,9 @@ setup(
     description='ROS 2 runtime for sim2sim and sim2real control of the Dodo robot.',
     license='TODO',
     entry_points={
-        'console_scripts': [],
-    },
+    "console_scripts": [
+        "inference_node = dodo_ros_control.inference_node:main",
+        "dummy_node = dodo_ros_control.dummy_node:main",
+    ],
+},
 )
